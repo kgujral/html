a522c29fde2488ad2a9bb6efe24b32d463168b47
Karan Gujral <karan@prontoitlabs.com>
2014-05-27 11:38:16 +0530

    filter and ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['hello', 'world']);
+angular.module('app', ['hello', 'world', 'app.filters']);
 
 angular.module('hello', [])
 .controller('HelloCtrl', function ($scope) {
@@ -16,4 +16,30 @@ angular.module('world', [])
 
 	$scope.content = "Akshay";
 
+});
+
+angular.module('app.filters', [])
+.filter('reverse', function() {
+  return function(sandeep) {
+    sandeep = sandeep || '';
+    var out = "";
+    for (var i = 0; i < sandeep.length; i++) {
+      out = sandeep.charAt(i) + out;
+    }
+    return out;
+  };
+})
+.filter('star', function() {
+  return function(input) {
+    input = input || '';
+    var out = "";
+    for (var i = 0; i < input.length; i++) {
+    	var character = input.charAt(i);
+    	if(character === ' ') {
+    		character = '*';
+    	}
+      out = out + character;
+    }
+    return out;
+  };
 });
